d0f3d4d140d6e65c6160ada8e3c1cdf46e476b83
leogdion <leogdion@brightdigit.com>
2020-12-01 10:19:02 -0500

    Setup Vapor, SwiftNIO, More Matrices; Issues #18, #19, #20, #21, #68

--- Package.swift
+++ Package.swift
@@ -127,8 +127,7 @@ let package = Package(
         "swift test --enable-code-coverage --enable-test-discovery --generate-linuxmain",
         "swift run swiftformat .",
         "swift run swiftlint autocorrect",
-        "swift run sourcedocs generate build --spm-module MistKit -c -r",
-        // "swift run swiftpmls mine",
+        "swift run sourcedocs generate build -cra",
         "git add .",
         "swift run swiftformat --lint .",
         "swift run swiftlint"
