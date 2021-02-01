0952ccfe626920cd117a57b2b7970588d239624f
leogdion <leogdion@brightdigit.com>
2021-02-01 09:32:18 -0500

    Release/0.2.2
    * refactoring MKDatabase.perform
    * working on refactoring MKDatabase
    * added recordname parser test
    * adding config factory, character map encoder, token manager tests
    * added test for MKDatabaseConnection,MKTokenManager,MKEncoder,FetchRecordQuery
    * added asset, location, double, and date
    * fixing MKAsset download url
    * added tests for asset, location, date, and double
    * added translator for reporting
    * fix TodoItem, remove old request from httpclient
    * Update .swiftlint.yml
    * allowing readonly token managers
    * adding MKStaticTokenManager.swift
    * making tests public
    * fixing macOS workflow file
    * adding more tests
    * adding more verbose logs (#94)
    * Feature/code quality (#95)
    * fixing linting issues #56

--- Package.swift
+++ Package.swift
@@ -120,7 +120,7 @@ let package = Package(
 #if canImport(PackageConfig)
   import PackageConfig
 
-  let requiredCoverage: Int = 30
+  let requiredCoverage: Int = 40
 
   let config = PackageConfiguration([
     "komondor": [
