085992c125db0a230dd6fd8c5eaf2a81df50f8dd
Leo Dion <leogdion@brightdigit.com>
2022-01-25 19:43:46 -0500

    added heartwitch product

--- Sources/BrightDigitKit/Publish/Nodes/Pages/Product.swift
+++ Sources/BrightDigitKit/Publish/Nodes/Pages/Product.swift
@@ -46,6 +46,7 @@ extension Product {
           .a(
             .href(productURL),
             .target(.blank),
+            .img(.src(logo)),
             .h3(.text(title))
           ),
           .ol(
@@ -204,12 +205,48 @@ extension Product {
 }
 
 enum Products {
+  static let heartwitch: Product =
+    .init(title: "Heartwitch", description: "Show your heart rate as you live stream with this watch app!", isOpenSource: false, logo: "/media/images/products/heartwitch/logo.svg", style: .square, screenshots: [
+      "/media/images/products/heartwitch/watch/002.ActiveWorkout.PNG",
+      "/media/images/products/heartwitch/web/001.LoginScreen.png",
+      "/media/images/products/heartwitch/livestream/Heartwitch-BOTW-HCG.png",
+      "/media/images/products/heartwitch/web/002.CodeScreen.png",
+      "/media/images/products/heartwitch/livestream/Heartwitch-SMK8D-RR.png",
+      "/media/images/products/heartwitch/livestream/Heartwitch-SMK8D-MC.png",
+      "/media/images/products/heartwitch/watch/001.StartWorkout.PNG",
+      "/media/images/products/heartwitch/livestream/Heartwitch-BOTW-SMG.png",
+      "/media/images/products/heartwitch/web/003.ActiveScreen.png",
+      "/media/images/products/heartwitch/livestream/Heartwitch-SMK8D-DKJ.png"
+
+    ], platforms: ["web", "watchOS"], technologies: ["Vapor", "HealthKit", "Heroku", "PostgreSQL"], productURL: "https://heartwitch.app/")
   static let all = [
-    Product(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing diam donec adipiscing tristique.", title: "Portrait", logo: URL(string: "/media/images/products/sample/logo.jpeg")!, style: .portrait, screenshots: [
-      URL(string: "/media/images/products/sample/screenshot.jpeg")!, URL(string: "/media/images/products/sample/screenshot.jpeg")!, URL(string: "/media/images/products/sample/screenshot.jpeg")!, URL(string: "/media/images/products/sample/screenshot.jpeg")!, URL(string: "/media/images/products/sample/screenshot.jpeg")!, URL(string: "/media/images/products/sample/screenshot.jpeg")!
-    ], pressCoverage: [
-      .init(source: "9TO5Mac", quote: "It's Greatest App Ever! You must give them your Money!!!", url: .init(string: "https://www.huxley.net/bnw/four.html")!, date: Date(timeIntervalSince1970: 1_445_803_696.0))
-    ], platforms: ["iPhone", "iPad", "Apple Watch", "Web", "Mac"], technologies: ["iPhone", "iPad", "Apple Watch", "Web", "Mac"], productURL: .init(string: "https://google.com")!, githubURL: .init(string: "https://google.com"))
+    Self.heartwitch,
+    Product(
+      title: "Portrait",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing diam donec adipiscing tristique.",
+      logo: "/media/images/products/sample/logo.jpeg",
+      style: .portrait,
+      screenshots: [
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg"
+      ],
+      pressCoverage: [
+        .init(
+          source: "9TO5Mac",
+          quote: "It's Greatest App Ever! You must give them your Money!!!",
+          url: "https://www.huxley.net/bnw/four.html",
+          date: Date(timeIntervalSince1970: 1_445_803_696.0)
+        )
+      ],
+      platforms: ["iPhone", "iPad", "Apple Watch", "Web", "Mac"],
+      technologies: ["iPhone", "iPad", "Apple Watch", "Web", "Mac"],
+      productURL: "https://google.com",
+      githubURL: "https://google.com"
+    )
   ]
 }
 
@@ -229,18 +266,18 @@ struct ProductBuilder: PageBuilder {
 }
 
 struct Product {
-  internal init(description: String, title: String, isOpenSource: Bool = false, logo: URL, style: Product.ScreenshotStyle = .default, screenshots: [URL] = [], pressCoverage: [Product.PressCoverage] = [], platforms: [String], technologies: [String], productURL: URL, githubURL: URL? = nil) {
+  internal init(title: String, description: String, isOpenSource: Bool = false, logo: String, style: Product.ScreenshotStyle = .default, screenshots: [String] = [], pressCoverage: [Product.PressCoverage] = [], platforms: [String], technologies: [String], productURL: String, githubURL: String? = nil) {
     self.description = description
     self.title = title
     self.isOpenSource = isOpenSource
-    self.logo = logo
+    self.logo = URL(string: logo)!
     self.style = style
     self.screenshots = screenshots
     self.pressCoverage = pressCoverage
     self.platforms = platforms
     self.technologies = technologies
-    self.productURL = productURL
-    self.githubURL = githubURL
+    self.productURL = URL(string: productURL)!
+    self.githubURL = githubURL.map { URL(string: $0)! }
   }
 
   enum ScreenshotStyle: String, Codable, Equatable {
@@ -248,18 +285,25 @@ struct Product {
   }
 
   struct PressCoverage: Codable, Equatable, Hashable {
+    internal init(source: String, quote: String, url: String, date: Date) {
+      self.source = source
+      self.quote = quote
+      self.url = URL(string: url)!
+      self.date = date
+    }
+
     let source: String
     let quote: String
     let url: URL
     let date: Date
   }
 
-  let description: String
   let title: String
+  let description: String
   let isOpenSource: Bool
   let logo: URL
   let style: ScreenshotStyle
-  let screenshots: [URL]
+  let screenshots: [String]
   let pressCoverage: [PressCoverage]
   let platforms: [String]
   let technologies: [String]
