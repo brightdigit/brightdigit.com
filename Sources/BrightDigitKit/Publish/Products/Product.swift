70f5933d6d9d007a99facedb9dec6ddca89c66dd
Leo Dion <leogdion@brightdigit.com>
2022-01-26 10:06:49 -0500

    refactoring files

--- Sources/BrightDigitKit/Publish/Products/Product.swift
+++ Sources/BrightDigitKit/Publish/Products/Product.swift
@@ -1,13 +1,36 @@
-//
-//  File.swift
-//  
-//
-//  Created by Leo Dion on 1/26/22.
-//
-
 import Foundation
 
 struct Product {
+  static let all: [Product] = [
+    .heartwitch,
+    Product(
+      title: "Portrait",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing diam donec adipiscing tristique.",
+      logo: "/media/images/products/sample/logo.jpeg",
+      style: .portrait,
+      screenshots: [
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg",
+        "/media/images/products/sample/screenshot.jpeg"
+      ],
+      pressCoverage: [
+        .init(
+          source: "9TO5Mac",
+          quote: "It's Greatest App Ever! You must give them your Money!!!",
+          url: "https://www.huxley.net/bnw/four.html",
+          date: "2015-10-15"
+        )
+      ],
+      platforms: ["iPhone", "iPad", "Apple Watch", "Web", "Mac"],
+      technologies: ["iPhone", "iPad", "Apple Watch", "Web", "Mac"],
+      productURL: "https://google.com",
+      githubURL: "https://google.com"
+    )
+  ]
+
   internal init(title: String, description: String, isOpenSource: Bool = false, logo: String, style: Product.ScreenshotStyle = .default, screenshots: [String] = [], pressCoverage: [Product.PressCoverage] = [], platforms: [String], technologies: [String], productURL: String, githubURL: String? = nil) {
     self.description = description
     self.title = title
@@ -27,11 +50,11 @@ struct Product {
   }
 
   struct PressCoverage: Codable, Equatable, Hashable {
-    internal init(source: String, quote: String, url: String, date: Date) {
+    internal init(source: String, quote: String, url: String, date: String) {
       self.source = source
       self.quote = quote
       self.url = URL(string: url)!
-      self.date = date
+      self.date = PiHTMLFactory.dateFormatter.date(from: date)!
     }
 
     let source: String
