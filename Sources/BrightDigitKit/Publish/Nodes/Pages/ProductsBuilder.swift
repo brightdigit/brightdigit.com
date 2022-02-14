0eb0ae7012e826013578794adb7049c7917c900c
Leo Dion <leogdion@brightdigit.com>
2022-02-14 15:57:03 -0500

    working on product setup

--- Sources/BrightDigitKit/Publish/Nodes/Pages/ProductsBuilder.swift
+++ Sources/BrightDigitKit/Publish/Nodes/Pages/ProductsBuilder.swift
@@ -2,102 +2,8 @@ import Foundation
 import Plot
 import Publish
 
-extension Product.PressCoverage {
-  static func listItem(_ pressCoverage: Self) -> Node<HTML.ListContext> {
-    .li(
-      .a(.href(pressCoverage.url), .target(.blank),
-         .element(named: "figure", nodes: [
-           .blockquote(
-             .attribute(named: "cite", value: pressCoverage.url.absoluteString),
-             .p(.text(pressCoverage.quote))
-           ),
-           .element(named: "figcaption", nodes: [
-             .span(
-               .class("name"),
-               .text("9To5Mac")
-             ),
-             .element(named: "cite", text: PiHTMLFactory.itemFormatter.string(from: pressCoverage.date))
-           ])
-         ]))
-    )
-  }
-}
-
-extension Product {
-  static func listItem(_ product: Product) -> Node<HTML.ListContext> {
-    product.listItem()
-  }
-
-  func listItem() -> Node<HTML.ListContext> {
-    .li(
-      .section(
-        .header(
-          .a(
-            .href(productURL),
-            .target(.blank),
-            .img(.src(logo)),
-            .h3(.text(title))
-          ),
-          .ol(
-            .class("links"),
-            .li(
-              .a(
-                .href(productURL),
-                .target(.blank),
-                .text("Product Page")
-              )
-            ),
-            .unwrap(githubURL) { githubURL in
-              .li(
-                .a(
-                  .href(githubURL),
-                  .target(.blank),
-                  .i(.class("flaticon-github")),
-                  .text("GitHub")
-                )
-              )
-            }
-          ),
-          .ol(
-            .class("platforms"),
-            .forEach(platforms) { platform in
-              .li(.text(platform))
-            }
-          )
-        ),
-        .main(
-          .p(.text(description)),
-          .ol(
-            .class("screenshots \(style.rawValue)"),
-            .forEach(screenshots) { screenshotURL in
-              .li(
-                .img(.src(screenshotURL))
-              )
-            }
-          ),
-          .ol(
-            .class("press-coverage"),
-            .forEach(pressCoverage, PressCoverage.listItem)
-          )
-        ),
-        .footer(
-          .section(
-            .h4(.text("Technologies")),
-            .ol(
-              .forEach(technologies) { tech in
-                .li(.text(tech))
-              }
-            )
-          )
-        )
-      )
-    )
-  }
-}
-
-
 struct ProductsBuilder: PageBuilder {
-  internal init(products: [Product] = Products.all) {
+  internal init(products: [Product] = Product.all) {
     self.products = products
   }
 
@@ -110,4 +16,3 @@ struct ProductsBuilder: PageBuilder {
     ]
   }
 }
-
--- Sources/BrightDigitKit/Publish/Products/Heartwitch.swift
+++ Sources/BrightDigitKit/Publish/Products/Heartwitch.swift
@@ -1,59 +1,28 @@
-//
-//  File.swift
-//  
-//
-//  Created by Leo Dion on 1/26/22.
-//
-
 import Foundation
-
 
 extension Product {
   static let heartwitch: Product =
-    .init(title: "Heartwitch", description: "Show your heart rate as you live stream with this watch app!", isOpenSource: false, logo: "/media/images/products/heartwitch/logo.svg", style: .square, screenshots: [
-      "/media/images/products/heartwitch/watch/002.ActiveWorkout.PNG",
-      "/media/images/products/heartwitch/web/001.LoginScreen.png",
-      "/media/images/products/heartwitch/livestream/Heartwitch-BOTW-HCG.png",
-      "/media/images/products/heartwitch/web/002.CodeScreen.png",
-      "/media/images/products/heartwitch/livestream/Heartwitch-SMK8D-RR.png",
-      "/media/images/products/heartwitch/livestream/Heartwitch-SMK8D-MC.png",
-      "/media/images/products/heartwitch/watch/001.StartWorkout.PNG",
-      "/media/images/products/heartwitch/livestream/Heartwitch-BOTW-SMG.png",
-      "/media/images/products/heartwitch/web/003.ActiveScreen.png",
-      "/media/images/products/heartwitch/livestream/Heartwitch-SMK8D-DKJ.png"
+    .init(title: "Heartwitch", description: """
+      Livestream Your Health Stats Straight From Your Apple Watch
 
-    ], platforms: ["web", "watchOS"], technologies: ["Vapor", "HealthKit", "Heroku", "PostgreSQL"], productURL: "https://heartwitch.app/")
-}
+      Heartwitch is a combination Apple Watch and Web App for sharing your health data online.
 
-enum Products {
-  
-  static let all : [Product] = [
-    .heartwitch,
-    Product(
-      title: "Portrait",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing diam donec adipiscing tristique.",
-      logo: "/media/images/products/sample/logo.jpeg",
-      style: .portrait,
-      screenshots: [
-        "/media/images/products/sample/screenshot.jpeg",
-        "/media/images/products/sample/screenshot.jpeg",
-        "/media/images/products/sample/screenshot.jpeg",
-        "/media/images/products/sample/screenshot.jpeg",
-        "/media/images/products/sample/screenshot.jpeg",
-        "/media/images/products/sample/screenshot.jpeg"
-      ],
-      pressCoverage: [
-        .init(
-          source: "9TO5Mac",
-          quote: "It's Greatest App Ever! You must give them your Money!!!",
-          url: "https://www.huxley.net/bnw/four.html",
-          date: Date(timeIntervalSince1970: 1_445_803_696.0)
-        )
-      ],
-      platforms: ["iPhone", "iPad", "Apple Watch", "Web", "Mac"],
-      technologies: ["iPhone", "iPad", "Apple Watch", "Web", "Mac"],
-      productURL: "https://google.com",
-      githubURL: "https://google.com"
-    )
-  ]
+      Video game live streaming has become a mainstream form of entertainment, with viewers liking to see how streamers behave and react to gameplay. With Heartwitch, viewers can see how the streamer reacts on a physical level.
+
+      See the streamer’s heart rate shift when hiding from a monster, driving a racecar around a tight bend, or as their favorite character dies in their arms.
+
+      Unlike other apps that capture user health data, there is no need for extra hardware. It uses the built-in sensors of the Apple Watch to detect your heart rate and send it through our servers directly to your web browser.
+
+    """, style: .square, screenshots: [
+      .at(path: "watch/002.ActiveWorkout.PNG"),
+      .at(path: "web/001.LoginScreen.png"),
+      .at(path: "livestream/Heartwitch-BOTW-HCG.png"),
+      .at(path: "web/002.CodeScreen.png"),
+      .at(path: "livestream/Heartwitch-SMK8D-RR.png"),
+      .at(path: "livestream/Heartwitch-SMK8D-MC.png"),
+      .at(path: "watch/001.StartWorkout.PNG"),
+      .at(path: "livestream/Heartwitch-BOTW-SMG.png"),
+      .at(path: "web/003.ActiveScreen.png"),
+      .at(path: "livestream/Heartwitch-SMK8D-DKJ.png")
+    ], platforms: [.watchOS, .web], technologies: [.vapor, .healthkit, .heroku, .postgreSQL], productURL: "https://heartwitch.app/")
 }
