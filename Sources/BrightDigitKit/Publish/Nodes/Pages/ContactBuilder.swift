0812449d1371a794a07f19b4b304d6ec2dd18f14
Leo Dion <leogdion@brightdigit.com>
2022-03-16 14:40:03 -0400

    fixing #38

--- Sources/BrightDigitKit/Publish/Nodes/Pages/ContactBuilder.swift
+++ Sources/BrightDigitKit/Publish/Nodes/Pages/ContactBuilder.swift
@@ -21,8 +21,7 @@ public extension Node where Context == HTML.BodyContext {
       .id("contact-us-form"),
       .main(
         .header(
-          .img(.class("rounded-lg"),
-               .src("http://placeimg.com/800/450/tech/CF994871-96B0-4257-A9F8-73FFDF87C015"))
+          .img(.src("/media/contact-us.svg"))
         ),
         .main(
           .div(
@@ -72,8 +71,7 @@ public extension Node where Context == HTML.BodyContext {
       .main(
         .header(
           .img(
-            .src("https://via.placeholder.com/1920x1080.png"),
-            .class("rounded-lg")
+            .src("/media/social-media.svg")
           )
         ),
         .main(
