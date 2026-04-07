c23333a68830d14ffa33cdcd3212b3488cd3056d
Leo Dion <leogdion@brightdigit.com>
2026-04-06 21:47:46 -0400

    git subrepo clone (merge) https://github.com/BrightDigit/NPMPublishPlugin.git Packages/BrightDigit/NPMPublishPlugin
    subrepo:
      subdir:   "Packages/BrightDigit/NPMPublishPlugin"
      merged:   "ee72ed2"
    upstream:
      origin:   "https://github.com/BrightDigit/NPMPublishPlugin.git"
      branch:   "main"
      commit:   "ee72ed2"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/Homebrew/brew"
      commit:   "3a946229d1"

---
+++ Packages/BrightDigit/NPMPublishPlugin/Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.8
+// swiftlint:disable explicit_acl explicit_top_level_acl
+
+import PackageDescription
+
+let package = Package(
+  name: "NPMPublishPlugin",
+  platforms: [.macOS(.v12)],
+  products: [
+    .library(
+      name: "NPMPublishPlugin",
+      targets: ["NPMPublishPlugin"]
+    )
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/johnsundell/publish.git",
+      from: "0.9.0"
+    ),
+    .package(
+      url: "https://github.com/johnsundell/shellout.git",
+      from: "2.3.0"
+    )
+  ],
+  targets: [
+    .target(
+      name: "NPMPublishPlugin",
+      dependencies: [
+        .product(name: "Publish", package: "publish"),
+        .product(name: "ShellOut", package: "shellout")
+      ]
+    ),
+    .testTarget(
+      name: "NPMPublishPluginTests",
+      dependencies: ["NPMPublishPlugin"]
+    )
+  ]
+)
